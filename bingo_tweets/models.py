e47146a233a8d9029dcd4675eaa63dbbc7896965
Alexander Schier <allo@laxu.de>
2016-10-21 23:24:24 +0200

    use try/except for TweepErrors, so that games can still be started when tweeting fails

--- bingo_tweets/models.py
+++ bingo_tweets/models.py
@@ -22,20 +22,24 @@ TWEET_TEXT_WITH_TOPIC = getattr(settings, "TWEET_TEXT_WITH_TOPIC",
 
 @receiver(post_save, sender=Game)
 def tweet_game(sender, instance, created, **kwargs):
-    game = instance
-    if created and TWITTER_API_KEY and TWITTER_API_SECRET and \
-       TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_TOKEN_SECRET:
-        auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-        auth.set_access_token(TWITTER_ACCESS_TOKEN,
-                              TWITTER_ACCESS_TOKEN_SECRET)
-        api = tweepy.API(auth)
-        if game.description:
-            tweet = unicode(TWEET_TEXT_WITH_TOPIC, "utf-8").format(
-                domain=game.site.domain,
-                absolute_url=game.get_absolute_url(),
-                topic=game.description)
-        else:
-            tweet = unicode(TWEET_TEXT, "utf-8").format(
-                domain=game.site.domain,
-                absolute_url=game.get_absolute_url())
-        api.update_status(tweet)
+    try:
+        game = instance
+        if created and TWITTER_API_KEY and TWITTER_API_SECRET and \
+           TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_TOKEN_SECRET:
+            auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+            auth.set_access_token(TWITTER_ACCESS_TOKEN,
+                                  TWITTER_ACCESS_TOKEN_SECRET)
+            api = tweepy.API(auth)
+            if game.description:
+                tweet = unicode(TWEET_TEXT_WITH_TOPIC, "utf-8").format(
+                    domain=game.site.domain,
+                    absolute_url=game.get_absolute_url(),
+                    topic=game.description)
+            else:
+                tweet = unicode(TWEET_TEXT, "utf-8").format(
+                    domain=game.site.domain,
+                    absolute_url=game.get_absolute_url())
+            api.update_status(status=tweet)
+    except tweepy.TweepError:
+        # TODO: log an error
+        pass
