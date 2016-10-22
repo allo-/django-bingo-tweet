87d30eaed073f36aef4886d1d33306fd362fa8fd
Alexander Schier <allo@laxu.de>
2016-10-22 16:31:45 +0200

    log an error, when tweeting failed

--- bingo_tweets/models.py
+++ bingo_tweets/models.py
@@ -22,24 +22,25 @@ TWEET_TEXT_WITH_TOPIC = getattr(settings, "TWEET_TEXT_WITH_TOPIC",
 
 @receiver(post_save, sender=Game)
 def tweet_game(sender, instance, created, **kwargs):
-    try:
-        game = instance
-        if created and TWITTER_API_KEY and TWITTER_API_SECRET and \
-           TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_TOKEN_SECRET:
+    game = instance
+    if created and TWITTER_API_KEY and TWITTER_API_SECRET and \
+       TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_TOKEN_SECRET:
+        if game.description:
+            tweet = unicode(TWEET_TEXT_WITH_TOPIC, "utf-8").format(
+                domain=game.site.domain,
+                absolute_url=game.get_absolute_url(),
+                topic=game.description)
+        else:
+            tweet = unicode(TWEET_TEXT, "utf-8").format(
+                domain=game.site.domain,
+                absolute_url=game.get_absolute_url())
+        try:
             auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
             auth.set_access_token(TWITTER_ACCESS_TOKEN,
                                   TWITTER_ACCESS_TOKEN_SECRET)
             api = tweepy.API(auth)
-            if game.description:
-                tweet = unicode(TWEET_TEXT_WITH_TOPIC, "utf-8").format(
-                    domain=game.site.domain,
-                    absolute_url=game.get_absolute_url(),
-                    topic=game.description)
-            else:
-                tweet = unicode(TWEET_TEXT, "utf-8").format(
-                    domain=game.site.domain,
-                    absolute_url=game.get_absolute_url())
             api.update_status(status=tweet)
-    except tweepy.TweepError:
-        # TODO: log an error
-        pass
+        except tweepy.TweepError:
+            import logging
+            logger = logging.getLogger("root")
+            logger.error("Could not tweet game {game_id} on site {domain}. Check your twitter credentials and twitter API status.".format(game_id=game.game_id, domain=game.site.domain))
