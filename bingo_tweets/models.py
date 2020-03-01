b10c46587f53467d9f93a821596db125834cae86
Alexander Schier <allo@laxu.de>
2020-03-01 15:13:14 +0100

    Moved the settings from settings.py to a TwitterConfig model
    Created TwitterAPIKey and TwitterConfig models, that allow to add per
    site Twitter configuration without having multiple settings.py files.

--- bingo_tweets/models.py
+++ bingo_tweets/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.sites.models import Site
 from django.utils.translation import ugettext as _
 
 import tweepy
@@ -20,27 +22,47 @@ TWEET_TEXT_WITH_TOPIC = getattr(settings, "TWEET_TEXT_WITH_TOPIC",
     _("New game: http://{domain:s}{absolute_url:s} (Topic: {topic:s})"))
 
 
+class TwitterAPIKey(models.Model):
+    name = models.CharField(max_length=255, help_text="A name for the API key "
+        "that allows you to distinguish it from other API keys.")
+    key = models.CharField(max_length=255)
+    secret = models.CharField(max_length=255)
+    access_token = models.CharField(max_length=255)
+    access_token_secret = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class TwitterConfig(models.Model):
+    site = models.OneToOneField(Site, unique=True)
+    api_key = models.ForeignKey(TwitterAPIKey)
+    tweet_text = models.CharField(max_length=255, default=_("New game: http://{domain:s}{absolute_url:s}"))
+    tweet_text_with_topic = models.CharField(max_length=255, default=_("New game: http://{domain:s}{absolute_url:s} (Topic: {topic:s})"))
+
+
 @receiver(post_save, sender=Game)
 def tweet_game(sender, instance, created, **kwargs):
     game = instance
-    if created and TWITTER_API_KEY and TWITTER_API_SECRET and \
-       TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_TOKEN_SECRET:
+    try:
+        config = TwitterConfig.objects.get(site=game.site)
         if game.description:
-            tweet = TWEET_TEXT_WITH_TOPIC.format(
+            tweet = config.tweet_text_with_topic.format(
                 domain=game.site.domain,
                 absolute_url=game.get_absolute_url(),
                 topic=game.description)
         else:
-            tweet = TWEET_TEXT.format(
+            tweet = config.tweet_text.format(
                 domain=game.site.domain,
                 absolute_url=game.get_absolute_url())
         try:
-            auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-            auth.set_access_token(TWITTER_ACCESS_TOKEN,
-                                  TWITTER_ACCESS_TOKEN_SECRET)
+            auth = tweepy.OAuthHandler(config.api_key.key, config.api_key.secret)
+            auth.set_access_token(config.api_key.access_token,
+                                  config.api_key.access_token_secret)
             api = tweepy.API(auth)
             api.update_status(status=tweet)
         except tweepy.TweepError:
             import logging
             logger = logging.getLogger("root")
             logger.error("Could not tweet game {game_id} on site {domain}. Check your twitter credentials and twitter API status.".format(game_id=game.game_id, domain=game.site.domain))
+    except ObjectDoesNotExist:
+        return
